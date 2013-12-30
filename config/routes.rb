231f974d40b42530c5b5dcebd63b9e6956aa2dbc
Jacelyn Chan <jacelyn_chan@hotmail.com>
2013-12-30 10:51:14 -0500

    Specified ruby version in gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Pinteresting::Application.routes.draw do
   
   root "pages#home"
   get "about" => "pages#about" # creates about_path
-  get "profiles" => "pages#profiles"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
